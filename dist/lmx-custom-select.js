0fe7d5bf7072c8f4f2cc6f2d2002ec4876c9ec8c
Anton Karmanov <anton@karmanov.ws>
2018-02-22 17:12:34 +0700

    changes: - [ADD] options for filtering - [FIX] class names

--- dist/lmx-custom-select.js
+++ dist/lmx-custom-select.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 angular.module('lmxCustomSelect', []).run(['$templateCache', function ($templateCache) {
-    var template = '\n            <div class="customSelect">\n                <div outside-click="close()">\n                    <input class="customSelect-selected" ng-class="{\'_disabled\': !model}" ng-click="trigger()" ng-value="value" type="text" readonly />\n\n                    <div class="customSelect-scroller__wrapper" ng-class="{\'_hide\': !isVisible}">\n                        <input class="customSelect-filter" type="text" ng-model="$parent.valueOfFilterByText" ng-if="filterByText" />\n                        \n                        <div class="customSelect-scroller">\n                            <ul class="customSelect-list">\n                                <li class="customSelect-item _disabled" ng-if="caption">{{caption}}</li>\n                                <li class="customSelect-item" ng-repeat="value in filteredRepeat = (repeat | filter:valueOfFilterByText)" ng-click="subValue ? select(value[subValue.value]) : select(value)">\n                                    {{subValue ? value[subValue.text] : value}}\n                                </li>\n                                <li class="customSelect-item _disabled" ng-if="!filteredRepeat.length">Filter is incorrect</li>\n                            </ul>\n                        </div>\n\n                        <div class="customSelect-scroller__track">\n                            <div class="customSelect-scroller__bar"></div>\n                        </div>\n                    </div>\n                 </div>\n\n                 <select ng-model="model" ng-options="{{options}}"></select>\n            </div>\n        ';
+    var template = '\n            <div class="customSelect">\n                <div outside-click="close()">\n                    <input class="customSelect-selected" ng-class="{\'_disabled\': !model}" ng-click="trigger()" ng-value="value" type="text" readonly />\n\n                    <div class="customSelect-scroller__wrapper" ng-class="{\'_hide\': !isVisible}">\n                        <div class="customSelect-filter__wrapper" ng-if="filterByText">\n                            <input class="customSelect-filter" type="text" ng-model="$parent.valueOfFilterByText" ng-attr-placeholder="{{filterByText.placeholder}}" />\n                        </div>\n                    \n                        <div class="customSelect-scroller">\n                            <ul class="customSelect-list">\n                                <li class="customSelect-item _caption" ng-if="caption">{{caption}}</li>\n                                <li class="customSelect-item" ng-repeat="value in filteredRepeat = (repeat | filter:valueOfFilterByText)" ng-click="subValue ? select(value[subValue.value]) : select(value)">\n                                    {{subValue ? value[subValue.text] : value}}\n                                </li>\n                                <li class="customSelect-item _incorrect" ng-if="!filteredRepeat.length">{{filterByText.noMatchFound}}</li>\n                            </ul>\n                        </div>\n\n                        <div class="customSelect-scroller__track">\n                            <div class="customSelect-scroller__bar"></div>\n                        </div>\n                    </div>\n                 </div>\n\n                 <select ng-model="model" ng-options="{{options}}"></select>\n            </div>\n        ';
 
     $templateCache.put('lmx-custom-select.html', template);
 }]).directive('lmxCustomSelect', function ($filter) {
@@ -16,13 +18,18 @@ angular.module('lmxCustomSelect', []).run(['$templateCache', function ($template
             nameVariableForOptions: '@repeat',
             options: '@',
             placeholder: '@',
-            caption: '@'
+            caption: '@',
+            filterByText: '<'
         },
         templateUrl: function templateUrl($element, $attrs) {
             return $attrs.templateUrl || 'lmx-custom-select.html';
         },
         link: function link($scope, elem, attrs, ngModelController) {
-            $scope.filterByText = attrs.filterByText !== undefined;
+            $scope.filterByText = attrs.filterByText === undefined ? false : _extends({
+                placeholder: 'Search',
+                noMatchFound: 'Filter is incorrect'
+            }, $scope.filterByText);
+
             $scope.valueOfFilterByText = '';
             $scope[$scope.nameVariableForOptions] = $scope.repeat;
             $scope.options = attrs.options.replace(attrs.repeat, "repeat");
