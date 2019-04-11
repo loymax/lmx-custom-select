30442a2c8097e2a47b5125579fa61e594e713544
e.kulesh <e.kulesh@loymax.ru>
2019-04-11 15:06:46 +0700

    [FIX] lodash vulnerability fix

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const shell = require('shelljs');
 
-if (shell.exec('babel index.js --out-file ./dist/lmx-custom-select.js').code !== 0) {
+if (shell.exec('npx babel index.js --out-file ./dist/lmx-custom-select.js').code !== 0) {
     shell.echo('Error development');
     shell.exit(1);
 } else {
-    if (shell.exec('uglifyjs dist/lmx-custom-select.js --compress --output dist/lmx-custom-select.min.js').code !== 0) {
+    if (shell.exec('uglifyjs ./dist/lmx-custom-select.js --compress --output ./dist/lmx-custom-select.min.js').code !== 0) {
         shell.echo('Error production');
         shell.exit(1);
     }
